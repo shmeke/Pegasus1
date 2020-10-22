50b5da2c44fc9760805015589951cc9d1af460fb
sebastian <sebastian.goransson@outlook.com>
2020-10-22 19:17:40 +0200

    Japp

--- app/src/main/java/com/sebastianstext/pegasus/MovementDetector.java
+++ app/src/main/java/com/sebastianstext/pegasus/MovementDetector.java
@@ -5,64 +5,68 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.FloatMath;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MovementDetector extends AppCompatActivity implements SensorEventListener {
+public class MovementDetector extends AppCompatActivity implements SensorEventListener{
 
-    private SensorManager sensorManager;
-    private Sensor accelerometer;
-    double[] maxAccelerations = new double[3];
-    double[] position = new double[3];
-    long[] times = new long[3];
-    // time combined with maxAcceleration can approximate the change in position,
-// with the formula Δpos = (maxAcceleration * time ^ 2) / 6
-    long currentTime;
+    private SensorManager sensorMan;
+    private Sensor motion;
+    private float[] mGravity;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+    private int stops;
 
-    protected void onCreate(Bundle savedInstanceState) {
+
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null) {
-            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-            sensorManager.registerListener(this, accelerometer, sensorManager.SENSOR_DELAY_FASTEST);
-        }
-        currentTime = System.currentTimeMillis();
-        for (int i = 0; i < 3; i++) {
-            times[i] = currentTime;
-        }
-
+        sensorMan = (SensorManager) getSystemService(SENSOR_SERVICE);
+        motion = sensorMan.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
     }
 
     @Override
-    public void onAccuracyChanged(Sensor ignore, int thisFunction) {
+    public void onResume() {
+        super.onResume();
+        sensorMan.registerListener(this, motion,
+                SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorMan.unregisterListener(this);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
-            for (int i = 0; i < 3; i++) {
-                if (Math.abs(event.values[i]) < 0.01) {
-                    // Note: this is to try to prevent accelerating time from being counted when the phone is stationary. 0.01 should be
-                    // changed to an appropriate sensitivity level that can be calculated by finding an average noise level when the phone is stationary.
-                    times[i] = System.currentTimeMillis();
-                }
-                if (event.values[i] > maxAccelerations[i] && maxAccelerations[i] >= 0) {
-                    maxAccelerations[i] = event.values[i];
-                } else if (event.values[i] < maxAccelerations[i] && maxAccelerations[i] <= 0) {
-                    maxAccelerations[i] = event.values[i];
-                } else if (event.values[i] > 0 && maxAccelerations[i] < 0) {
-                    currentTime = System.currentTimeMillis();
-                    position[i] += maxAccelerations[i] * (times[i] - currentTime) * (times[i] - currentTime) / 6;
-                    times[i] = currentTime;
-                    maxAccelerations[i] = event.values[i];
-                } else if (event.values[i] < 0 && maxAccelerations[i] > 0) {
-                    currentTime = System.currentTimeMillis();
-                    position[i] += maxAccelerations[i] * (times[i] - currentTime) * (times[i] - currentTime) / 6;
-                    times[i] = currentTime;
-                    maxAccelerations[i] = event.values[i];
-                }
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            mGravity = event.values.clone();
+            // Shake detection
+            float x = mGravity[0];
+            float y = mGravity[1];
+            float z = mGravity[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt(x*x + y*y + z*z);
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+            // Make this higher or lower according to how much
+            // motion you want to detect
+            if(mAccel > 0.01){
+                //Device is moving
+            } else {
+                //Device is not moving
             }
         }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
--- app/src/main/java/com/sebastianstext/pegasus/WorkoutActivity.java
+++ app/src/main/java/com/sebastianstext/pegasus/WorkoutActivity.java
@@ -2,16 +2,10 @@ package com.sebastianstext.pegasus;
 
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -21,40 +15,24 @@ import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintSet;
 
 
-@RequiresApi(api = Build.VERSION_CODES.O)
-public class WorkoutActivity extends AppCompatActivity implements SensorEventListener, StepListener {
+public class WorkoutActivity extends AppCompatActivity {
 
-    TextView Distance, Speed;//, NumberStops, DistTrav, DistSkritt, DistGallopp, VoltRight, VoltLeft;
+    //NumberStops, DistTrav, DistSkritt, DistGallopp, VoltRight, VoltLeft;
     Button buttonProf;
-    private Intent intent;
-    private StepDetector simpleStepDetector;
-    private SensorManager sensorManager;
-    private Sensor accel;
-    private Sensor motion;
-    private  MovementDetector motionDetector;
-    private static final String TEXT_NUM_STEPS = "Number of Steps: ";
-    private int numSteps;
-    private Button BtnStart;
-    private double meters;
-    private double speed;
-    private long time;
-    private double km;
-    static Instant Start;
+    Button BtnStart;
 
 
 
-    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
-    LocalDateTime now = LocalDateTime.now();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.workout_activity);
+        setContentView(R.layout.activity_menu);
         buttonProf = findViewById(R.id.buttonProfile);
-        Distance = findViewById(R.id.textViewDist);
-        Speed = findViewById(R.id.textViewSpeed);
+        BtnStart = (Button) findViewById(R.id.buttonStart);
+
         /*NumberStops = findViewById(R.id.textViewStopp);
         DistTrav = findViewById(R.id.textViewDistTrav);
         DistSkritt = findViewById(R.id.textViewDistSkritt);
@@ -62,13 +40,7 @@ public class WorkoutActivity extends AppCompatActivity implements SensorEventLis
         VoltRight = findViewById(R.id.textViewNmbrVoltRight);
         VoltLeft = findViewById(R.id.textViewNmbrVoltLeft);
 */    // Get an instance of the SensorManager
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        motion = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        simpleStepDetector = new StepDetector();
-        motionDetector = new MovementDetector();
-        simpleStepDetector.registerListener(this);
-        BtnStart = (Button) findViewById(R.id.buttonStart);
+
         buttonProf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -78,62 +50,16 @@ public class WorkoutActivity extends AppCompatActivity implements SensorEventLis
             }
         });
         BtnStart.setOnClickListener(new View.OnClickListener() {
+         @Override
+            public void onClick(View view) {
+              Intent a = new Intent(WorkoutActivity.this, DistSpeedActivity.class);
+                startActivity(a);
 
-            @Override
-            public void onClick(View arg0) {
-
-                numSteps = 0;
-                Instant start = Instant.now();
-                Start = start;
-                sensorManager.registerListener(WorkoutActivity.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
-
+                /*Intent b = new Intent(WorkoutActivity.this, MovementDetector.class);
+                startActivity(b);*/
             }
         });
-
-
-
     }
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            simpleStepDetector.updateAccel(
-                    event.timestamp, event.values[0], event.values[1], event.values[2]);
-        }
-
-
-    }
-
-
-    @Override
-    public void step(long timeNs) {
-        numSteps++;
-        meters = (numSteps);
-        Instant still = Instant.now();
-        Duration timeElapsed = Duration.between(
-                Start,
-                still
-        );
-
-        float sec = timeElapsed.toMillis();
-        float timeSeconds = sec;
-
-        km =  (meters*0.001);
-
-        if(meters > 1000){
-            Distance.setText(km+ " km");
-        }else{
-            Distance.setText(meters+ " m");
-        }
-        double kps = (km) / (timeSeconds*3600);
-
-        // Speed.setText((int) speed+ " km/h");
-        Speed.setText(kps+ " km/h");
-    }
-
 
 
 }
