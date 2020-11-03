3bdc5b88e948b36cddbbf7a5ac545f3bc90a5816
shmeke <67658976+shmeke@users.noreply.github.com>
2020-11-03 13:41:21 +0100

    Add files via upload

---
+++ DbConnect.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$database = "pegasus";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
